ed23517f9e6c483622999c00a0224af2e2ed000f
GRiker <griker@hotmail.com>
2013-08-04 10:04:33 -0600

    Cleaning up, tweaked help file with updated images

---
+++ dialogs/deep_view_items.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__ = 'GPL v3'
+__copyright__ = '2010, Gregory Riker'
+__docformat__ = 'restructuredtext en'
+
+import re, sys
+
+from calibre.devices.usbms.driver import debug_print
+from calibre.gui2 import warning_dialog
+
+from calibre_plugins.marvin_manager.book_status import dialog_resources_path
+
+from PyQt4.Qt import (QDialog, QDialogButtonBox, QIcon, QPixmap,
+                      QSize)
+
+# Import Ui_Form from form generated dynamically during initialization
+if True:
+    sys.path.insert(0, dialog_resources_path)
+    from deep_view_items_ui import Ui_Dialog
+    sys.path.remove(dialog_resources_path)
+
+
+class DeepViewItems(QDialog, Ui_Dialog):
+    '''
+    Present user with a list of DV items
+    items keys(): ['ID', 'Name', 'Cnt', 'Loc', 'Flag', 'Note', 'Confidence']
+    '''
+
+    LOCATION_TEMPLATE = "{cls}:{func}({arg1}) {arg2}"
+
+
+    def __init__(self, parent, title, items, verbose=True):
+        QDialog.__init__(self, parent)
+        self.items = items
+        self.parent = parent
+        self.result = None
+
+        self.setupUi(self)
+        self.verbose = verbose
+        self._log_location()
+
+        # Set title
+        self.dvi_gb.setTitle(title)
+
+        # Populate the combobox
+        for item in items:
+            self.dvi_cb.addItem("%s (%d)" % (item[b'Name'], item[b'Cnt']),
+                                item[b'ID'])
+
+        # Retrieve saved hit count
+        hits = self.parent.prefs.get('deep_view_hits', 5)
+        self.dvi_sb.setValue(hits)
+
+        # Hook the button events
+        self.bb.clicked.connect(self.dispatch_button_click)
+
+    def accept(self):
+        self._log_location()
+        index = self.dvi_cb.currentIndex()
+        item = re.sub("\s{1}\(\d+\)", '', str(self.dvi_cb.currentText()))
+        self.result = {'ID':  str(self.dvi_cb.itemData(index).toString()),
+                       'hits': str(self.dvi_sb.value()),
+                       'item': item}
+
+        # Save the hit count
+        self.parent.prefs.set('deep_view_hits', int(self.dvi_sb.value()))
+
+        super(DeepViewItems, self).accept()
+
+    def close(self):
+        self._log_location()
+        self.result = None
+
+        # Save the hit count
+        self.parent.prefs.set('deep_view_hits', int(self.dvi_sb.value()))
+
+        super(DeepViewItems, self).close()
+
+    def dispatch_button_click(self, button):
+        '''
+        BUTTON_ROLES = ['AcceptRole', 'RejectRole', 'DestructiveRole', 'ActionRole',
+                        'HelpRole', 'YesRole', 'NoRole', 'ApplyRole', 'ResetRole']
+        '''
+        self._log_location()
+        if self.bb.buttonRole(button) == QDialogButtonBox.AcceptRole:
+            # Save content
+            self.accept()
+
+        elif self.bb.buttonRole(button) == QDialogButtonBox.RejectRole:
+            # Cancelled
+            self.close()
+
+    def esc(self, *args):
+        self.cancel()
+
+    # ~~~~~~ Helpers ~~~~~~
+    def _log(self, msg=None):
+        '''
+        Print msg to console
+        '''
+        if not self.verbose:
+            return
+
+        if msg:
+            debug_print(" %s" % msg)
+        else:
+            debug_print()
+
+    def _log_location(self, *args):
+        '''
+        Print location, args to console
+        '''
+        if not self.verbose:
+            return
+
+        arg1 = arg2 = ''
+
+        if len(args) > 0:
+            arg1 = args[0]
+        if len(args) > 1:
+            arg2 = args[1]
+
+        debug_print(self.LOCATION_TEMPLATE.format(
+            cls=self.__class__.__name__,
+            func=sys._getframe(1).f_code.co_name,
+            arg1=arg1, arg2=arg2))
