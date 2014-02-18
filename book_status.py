9457f96b9cbcb73f49d277aead39ecbcad42645b
Gregory Riker <griker@hotmail.com>
2014-02-18 05:55:50 -0700

    Add meta tag to tell calibre not to sanitize when viewing with comments editor

--- book_status.py
+++ book_status.py
@@ -4205,6 +4205,7 @@ class BookStatusDialog(SizePersistedDialog, Logger):
                 '<html xmlns="http://www.w3.org/1999/xhtml">'
                 '<head>'
                 '<meta http-equiv="content-type" content="text/html; charset=utf-8"/>'
+                '<meta name="calibre-dont-sanitize">'
                 '<title>Annotations</title>'
                 '<style></style>'
                 '</head>'
